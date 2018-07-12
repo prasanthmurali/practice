f0219750019f704c376017be8ff4da498ec9268c
prasanth <prasanth@Prasanths-MacBook-Air.local>
2018-07-11 22:55:59 -0400

    placeholder: count no of 0s

---
+++ algorithms/noOfZeros.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+/*
+ * Given an array of 1s and 0s which has all 1s first followed by all 0s. Find the number of 0s. Count the number of zeroes in the given array.
+
+Examples :
+
+Input: arr[] = {1, 1, 1, 1, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 0, 0, 0, 0}
+Output: 4
+
+Input: arr[] = {0, 0, 0}
+Output: 3
+
+Input: arr[] = {1, 1, 1, 1}
+Output: 0
+Ref: https://www.geeksforgeeks.org/find-number-zeroes/
+ */
+public class noOfZeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
